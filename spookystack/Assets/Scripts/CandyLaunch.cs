e8a5ab56e878deeb516847ca2152a662567c9c59
Anh Chi Bui <acqbui@gmail.com>
2014-10-24 19:34:38 -0400

    CandyLaunch script

---
+++ spookystack/Assets/Scripts/CandyLaunch.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CandyLaunch : MonoBehaviour {
+
+	public GameObject aTarget; 
+	public string aTargetNamePrefix; 
+	public float aSpeed;
+	public float aAccel; 
+
+	/* --------------------------------------------------------------------------------------------------------------------
+	 * START: 
+	 * find the target: opponent's bottom hat
+	 * -------------------------------------------------------------------------------------------------------------------- */
+	void Start () {
+		aTarget = GameObject.Find (aTargetNamePrefix + " 1"); 
+	}
+
+	/* --------------------------------------------------------------------------------------------------------------------
+	 * FIXED UPDATE:
+	 * - slerp the candy towards the opponent's bottom hat
+	 * - accelerate the slerp
+	 * -------------------------------------------------------------------------------------------------------------------- */
+	void FixedUpdate()
+	{
+		transform.position = Vector3.Slerp (transform.position, aTarget.transform.position, aSpeed * Time.deltaTime); 
+		aSpeed += aAccel; 
+	}
+}
