e8ea597c6de3896ba39f758d2010af01bc7a5345
Anh Chi Bui <acqbui@gmail.com>
2014-10-24 20:23:18 -0400

    Scripts and meta files

---
+++ spookystack/Assets/Scripts/HatDealer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HatDealer : MonoBehaviour {
+
+	public int aHatCost; 
+
+	public int minCost;
+	public int maxCost; 
+
+	// Use this for initialization
+	void Start () {
+		aHatCost = Random.Range (minCost, maxCost+1); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ spookystack/Assets/Scripts/House.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class House : MonoBehaviour {
+
+	public Player aPlayer; 
+	public Material[] aMaterials; 
+	
+	public string aNamePrefix; 
+	public int aID; 
+
+	public float aVShift;
+	public float aHShift; 
+	public float aSpeed; 
+	public Vector3 aShiftPos;
+	public bool aShiftHouse; 
+
+	// Use this for initialization
+	void Start () {
+		setMaterial(); 
+		gameObject.name = aNamePrefix + (aPlayer.aHouses + 1);
+		aShiftHouse = false; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/* --------------------------------------------------------------------------------------------------------------------
+	 * FIXED UPDATE: 
+	 * Handles the house position lerp if the bottom house is destroyed 
+	 * -------------------------------------------------------------------------------------------------------------------- */
+	void FixedUpdate()
+	{
+		if (aShiftHouse)
+		{
+			transform.position = Vector3.Lerp (transform.position, aShiftPos, Time.deltaTime * aSpeed);
+			
+			if (transform.position.y <= aShiftPos.y + 0.05f )
+			{
+				if (aID == 0)
+				{
+
+				}
+				transform.position = new Vector3 (transform.position.x, aShiftPos.y, transform.position.z);
+				aShiftHouse = false; 
+			}
+		}
+	}
+
+	/* --------------------------------------------------------------------------------------------------------------------
+	 * SHIFT HOUSE:
+	 * when the player finishes with bottom house, shift all the houses  
+	 * - change this hat's id
+	 * - change this hat's name 
+	 * - trigger shift of the hat  
+	 * -------------------------------------------------------------------------------------------------------------------- */
+	public void shiftHouse()
+	{
+		if (aID == 1)
+		{
+			aID--; 
+			gameObject.name = aNamePrefix + aID; 
+			aShiftPos = new Vector3 (transform.position.x, transform.position.y - aVShift, transform.position.z); 
+			aShiftHouse = true; 
+		}
+		else 
+		{
+			aID--;
+			gameObject.name = aNamePrefix + aID; 
+			aShiftPos = new Vector3 (transform.position.x, transform.position.y - aVShift, transform.position.z); 
+			aShiftHouse = true; 
+		}
+	}
+
+	/* --------------------------------------------------------------------------------------------------------------------
+	 * SET MATERIAL:
+	 * sets the house's material/colour depending on the house the player is at
+	 * -------------------------------------------------------------------------------------------------------------------- */
+	void setMaterial()
+	{
+		gameObject.renderer.material = aMaterials [aPlayer.aHouses]; 
+	}
+}
