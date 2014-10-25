d7863ec2b08c6d24623c67a3bd46868d84492bc9
unknown <1231286@5B1-04.ad.dawsoncollege.qc.ca>
2014-10-25 14:02:19 -0400

    Main Menu script, Layout script

---
+++ spookystack/Assets/Scripts/Labels.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Labels : MonoBehaviour {
+
+	public TextMesh Label1;
+	public TextMesh Label2;
+	public GameObject P1;
+	public GameObject P2;
+
+	// Use this for initialization
+	void Start () {
+
+		Label1.GetComponent<MeshRenderer> ().sortingLayerID = 8;
+		Label2.GetComponent<MeshRenderer> ().sortingLayerID = 8;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	
+	{
+		Label1.text = P1.GetComponent<Player>().aCandies.ToString(); 
+
+		Label2.text = P2.GetComponent<Player>().aCandies.ToString(); 
+
+	}
+}
---
+++ spookystack/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	void OnMouseDown () {
+	Application.LoadLevel("main");
+	}
+}
