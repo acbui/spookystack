05a8be5ff9199c4c7c77f4a82afa8b98554f5397
unknown <0933952@5B1-06.ad.dawsoncollege.qc.ca>
2014-10-25 13:19:30 -0400

    Prefab Updates

---
+++ spookystack/Assets/Scripts/HatRef.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HatRef : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		print ("CANDY");
+		Destroy (col.gameObject);
+	}
+}
