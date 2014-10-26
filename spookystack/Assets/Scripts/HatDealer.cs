83c97403417b8b3b279d3a632cf2cc7fd38b8311
acbui <acqbui@gmail.com>
2014-10-26 13:21:23 -0400

    Hat dealers fade out and the cost range varies with number of hats

--- spookystack/Assets/Scripts/HatDealer.cs
+++ spookystack/Assets/Scripts/HatDealer.cs
@@ -10,46 +10,72 @@ public class HatDealer : MonoBehaviour {
 	public int minCost;
 	public int maxCost; 
 
+	public bool fadingOut; 
+	public SpriteRenderer aRender;
+	public float aSpeed; 
+
 	// Use this for initialization
 	void Start () {
-		aHatCost = Random.Range (minCost, maxCost+1); 
+		fadingOut = false; 
+		aRender = gameObject.GetComponent<SpriteRenderer>();
+		aHatCost = Random.Range (minCost, (maxCost+1)*((aPlayer.aHats%5)+1)); 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (aID == 1)
+		if (!fadingOut)
 		{
-			if (Input.GetKeyDown (KeyCode.D))
+			if (aID == 1)
 			{
-				if (aPlayer.aCandies >= aHatCost)
+				if (Input.GetKeyDown (KeyCode.D))
 				{
-					aPlayer.aCandies -= aHatCost; 
-					aPlayer.aHats++;
-					aPlayer.makeHat (); 
-					Destroy (gameObject); 
+					if (aPlayer.aCandies >= aHatCost)
+					{
+						aPlayer.aCandies -= aHatCost; 
+						aPlayer.aHats++;
+						aPlayer.makeHat (); 
+						fadingOut = true; 
+						//Destroy (gameObject); 
+					}
+				}
+				if (Input.GetKeyDown (KeyCode.A))
+				{
+					fadingOut = true; 
+					//Destroy (gameObject); 
 				}
 			}
-			if (Input.GetKeyDown (KeyCode.A))
+
+			if (aID == 2)
 			{
-				Destroy (gameObject); 
+				if (Input.GetKeyDown (KeyCode.RightArrow))
+				{
+					if (aPlayer.aCandies >= aHatCost)
+					{
+						aPlayer.aCandies -= aHatCost; 
+						aPlayer.aHats++;
+						aPlayer.makeHat (); 
+						fadingOut = true; 
+						//Destroy (gameObject); 
+					}
+				}
+				if (Input.GetKeyDown (KeyCode.LeftArrow))
+				{
+					fadingOut = true; 
+					//Destroy (gameObject); 
+				}
 			}
 		}
+	}
 
-		if (aID == 2)
+	void FixedUpdate()
+	{
+		if (fadingOut)
 		{
-			if (Input.GetKeyDown (KeyCode.RightArrow))
+			aRender.color = Color.Lerp (aRender.color, new Color (aRender.color.r, aRender.color.g, aRender.color.b, 0), aSpeed*Time.deltaTime);
+
+			if (aRender.color.a <= 0.05f)
 			{
-				if (aPlayer.aCandies >= aHatCost)
-				{
-					aPlayer.aCandies -= aHatCost; 
-					aPlayer.aHats++;
-					aPlayer.makeHat (); 
-					Destroy (gameObject); 
-				}
-			}
-			if (Input.GetKeyDown (KeyCode.LeftArrow))
-			{
-				Destroy (gameObject); 
+				Destroy (gameObject);
 			}
 		}
 	}
